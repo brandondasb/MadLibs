cc3a82c6d03f56637ef589c4eeb50aa1e11d2dfb
Brandon.Milambo <brandonmilambo.dev@gmail.com>
2018-12-23 20:49:44 +0000

    boom

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scan = new Scanner(System.in);
+        String color="";
+        String pastTenseVerb ="";
+        String adjective= "";
+        String noun= "";
+
+        System.out.println("Please enter a color");
+        color= scan.nextLine();
+        System.out.println("Please enter a past tense verb");
+        pastTenseVerb = scan.nextLine();
+        System.out.println("Please enter a adjective");
+        adjective = scan.nextLine();
+        System.out.println("Please enter a noun");
+        noun = scan.nextLine();
+        System.out.print("The "+color+" Dragon "+pastTenseVerb+" at the "+adjective);
+
+        System.out.println(" Knight, who rode in on a sturdy, giant "+ noun);
+    }
+}
